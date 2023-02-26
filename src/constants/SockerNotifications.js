5cac806d52fa38575c47f1e2517aebddcbdf64ea
Ismar Slomic <ismar@slomic.no>
2023-02-26 18:06:22 +0100

    fix: remove JavaScript files accidentally tracked

--- src/constants/SockerNotifications.js
+++
@@ -1,6 +0,0 @@
-export var SocketNotification;
-(function (SocketNotification) {
-    SocketNotification["GREETINGS_TEXT_REQUEST"] = "GREETINGS_TEXT_REQUEST";
-    SocketNotification["GREETINGS_TEXT_RESPONSE"] = "GREETINGS_TEXT_RESPONSE";
-})(SocketNotification || (SocketNotification = {}));
-//# sourceMappingURL=SockerNotifications.js.map
