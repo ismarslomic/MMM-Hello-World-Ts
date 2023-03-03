4014c3c6d4f5ee930b74f5c5bb6dd9c8c8eb9de1
Ismar Slomic <ismar@slomic.no>
2023-03-03 12:43:02 +0100

    fix(test): update selector in e2e test

--- __tests__/e2e/greetings.cy.ts
+++ __tests__/e2e/greetings.cy.ts
@@ -4,7 +4,7 @@ describe('MMM-Hello-World-Ts', () => {
   })
 
   it('greetings message is displayed', () => {
-    cy.get('div[id$="MMM-Hello-World-Ts"] > div > div > div[class="bright light medium"]')
+    cy.get('div[id$="MMM-Hello-World-Ts"] > div > div > div[class="bright light medium green"]')
       .should('have.text', 'MMM-Hello-World-Ts says: Hello world Ismar!')
       .should('be.visible')
   })
