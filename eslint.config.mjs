5546e88679af2d7387485ba226e156f49cfda42d
Ismar Slomic <ismar@slomic.no>
2024-08-19 08:18:13 +0200

    chore(deps-dev): bump @typescript-eslint/parser to v8.1.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,6 +24,15 @@ export default [
       '@typescript-eslint': typescriptEslint,
     },
 
+    rules: {
+      '@typescript-eslint/no-require-imports': [
+        'error',
+        {
+          allowAsImport: true,
+        },
+      ],
+    },
+
     languageOptions: {
       globals: {
         ...globals.browser,
